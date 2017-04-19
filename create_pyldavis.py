588c6763f1fa23854c6704fbb942d06530927de2
Matt <md3es@virginia.edu>
2017-04-19 13:22:43 -0400

    re-direct create pyldavis file

--- create_pyldavis.py
+++ create_pyldavis.py
@@ -8,11 +8,11 @@ Created on Mon Mar 20 14:40:00 2017
 from gensim import corpora, models, similarities
 import pyLDAvis
 
-dictionary = corpora.Dictionary.load('C:/Users/Nick/Desktop/DSI Spring 2017/capstone/capstoneflaskapp/static/reddit.dict')
-corpus = corpora.MmCorpus('C:/Users/Nick/Desktop/DSI Spring 2017/capstone/capstoneflaskapp/static/reddit.mm')
-lda = models.ldamodel.LdaModel.load('C:/Users/Nick/Desktop/DSI Spring 2017/capstone/capstoneflaskapp/static/lda_reddit.model')
+dictionary = corpora.Dictionary.load('./models/FINAL416.dict')
+corpus = corpora.MmCorpus('./models/FINAL416.mm')
+lda = models.ldamodel.LdaModel.load('./models/FINAL50_416.model')
 
 import pyLDAvis.gensim
 
 p = pyLDAvis.gensim.prepare(lda, corpus, dictionary)
-pyLDAvis.save_html(p, 'C:/Users/Nick/Desktop/pyldavis_test.html')
+pyLDAvis.save_html(p, 'pyldavis_test.html')
